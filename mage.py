c6015e6d735bd8021f3b8085d81eede663231737
hashir500 <202951529+hashir500@users.noreply.github.com>
2026-03-26 23:54:52 +0500

    Add Mage class

---
+++ mage.py
@@ -0,0 +1,27 @@
+# importing character class
+from character import Character
+
+# initilizing warrior class
+class Mage(Character):
+    def __init__(self, name):
+        super().__init__(name, health= 80, max_health = 80, attack_power = 30, speed = 7)
+
+        self._mana = 100
+        self._mana_cost = 25
+    
+    def cast_spell(self,target):
+        if self._mana >= self._mana_cost:
+            self._mana = self._mana - self._mana_cost
+            return self._attack_power * 1.5
+        else:
+            print("Not sufficent mana")
+            return
+        
+    def regenerate_mana(self,amount):
+        self._mana += amount
+        self._mana = min(self._mana,100)
+        print(f"{self._name} generated {amount} mana.")
+
+    def get_status(self):
+        status = super().get_status()
+        print(status + f" | Mana: {self._mana}/100")
